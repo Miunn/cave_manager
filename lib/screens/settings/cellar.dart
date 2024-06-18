faeb3d5885761f8a3722bb89fea787772f08004e
Miunn <remcaulier@orange.fr>
2024-06-18 18:56:20 +0200

    Cellar summary on cellar settings

--- lib/screens/settings/cellar.dart
+++ lib/screens/settings/cellar.dart
@@ -1,5 +1,7 @@
+import 'package:cave_manager/providers/clusters_provider.dart';
+import 'package:cave_manager/widgets/cellarConfiguration/cellar_summary.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
 
 class SettingsCellar extends StatefulWidget {
   const SettingsCellar({super.key});
@@ -9,34 +11,6 @@ class SettingsCellar extends StatefulWidget {
 }
 
 class _SettingsCellarState extends State<SettingsCellar> {
-  int cellarWidth = 0;
-  int cellarHeight = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    loadCellarConfiguration();
-  }
-
-  loadCellarConfiguration() async {
-    final prefs = await SharedPreferences.getInstance();
-
-    setState(() {
-      cellarWidth = prefs.getInt('cellarWidth') ?? 0;
-      cellarHeight = prefs.getInt('cellarHeight') ?? 0;
-    });
-  }
-
-  wipeConfiguration() async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.remove('cellarType');
-    prefs.remove('cellarWidth');
-    prefs.remove('cellarHeight');
-    prefs.remove('cellarBags');
-    prefs.remove('cellarBagCapacity');
-    loadCellarConfiguration();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,11 +26,22 @@ class _SettingsCellarState extends State<SettingsCellar> {
           padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
           child: Column(
             children: <Widget>[
-              Text("Largeur de la cave : $cellarWidth"),
-              Text("Hauteur de la cave : $cellarHeight"),
-              ElevatedButton(
-                  onPressed: wipeConfiguration,
-                  child: const Text('Wipe configuration')),
+              Consumer(builder: (BuildContext context,
+                  ClustersProvider clusters, Widget? child) {
+                return CellarSummary(
+                    cellarType: clusters.cellarType,
+                    clustersConfiguration: clusters.clusters);
+              }),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: FilledButton(
+                  onPressed: () {
+
+                  },
+                  child: const Text("Supprimer"),
+                ),
+              )
             ],
           ),
         ));
