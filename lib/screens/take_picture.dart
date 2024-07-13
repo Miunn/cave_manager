6654adbaf0708491bf4febd0784fa30e5813bcb0
Miunn <remcaulier@orange.fr>
2024-07-13 18:42:25 +0200

    High resolution preset

--- lib/screens/take_picture.dart
+++ lib/screens/take_picture.dart
@@ -23,7 +23,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       // Get a specific camera from the list of available cameras.
       widget.camera,
       // Define the resolution to use.
-      ResolutionPreset.medium,
+      ResolutionPreset.high,
     );
     _controller.setFlashMode(flashMode);
 
